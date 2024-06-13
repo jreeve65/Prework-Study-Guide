ec3ce9030086d8ef02c7b29823c6d659514ed331
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-06-13 16:33:32 -0500

    fixed CSS file, Added JavaScript notes to HTML file, and added to JS file.

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,19 +1,45 @@
 const topics =['HTML','CSS','Git','JavaScript'];
-/*
-if (topic === 'HTML') {
+
+const randomTopic = topics[Math.floor(Math.random()*topics.length)];
+
+function listTopics() {
+    
+    for(let i =0; i<topics.length; i++) {
+        console.log(topics[i]);
+    }
+}
+
+
+function selectTopic() {
+
+    
+    if (randomTopic === 'HTML') {
+
     console.log("Let's study HTML!");
-} else if(topic === 'CSS'){
+
+    } else if(randomTopic === 'CSS'){
+
     console.log("Let's study CSS!");
-} else if(topic === "Git"){
+
+    } else if(randomTopic === "Git"){
+
     console.log("Let's study Git!");
 
-} else if(topic === "JavaScript") {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log('Please try again!');
-}
-  */  
+    } else if(randomTopic === "JavaScript") {
 
-for(let i =0; i<topics.length; i++) {
-    console.log(topics[i]);
+    console.log("Let's study JavaScript!");
+    }
+     else {
+    console.log('Please try again!');
+    
+    }     
+
 }
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+
+selectTopic();
+
+
